c022ad79aa37da1254410b50ffdbf3961b280d48
erdac <tonrc87@gmail.com>
2020-07-30 00:23:16 -0300

    Created the LogoForflix component

---
+++ src/components/LogoForflix/index.js
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+import logo from '../../assets/img/Logo.svg';
+
+const LogoForflix = styled.img.attrs({src: logo, alt: 'Logo do forflix'}) `
+    height: 40px;
+`;
+
+export default LogoForflix;
